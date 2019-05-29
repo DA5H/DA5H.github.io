4e70fed5ae473041a3d10a38b17aa311d99a142e
Xavier García <sistemas@protonmail.com>
2019-05-29 12:34:01 -0500

    1

---
+++ arriba.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function(){
+
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 150 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+
+});
+
+/*
+$(document).ready(function(){
+
+	$('.izettle_boton').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 500 ){
+			$('.izettle_boton').slideDown(300);
+		} else {
+			$('.izettle_boton').slideUp(300);
+		}
+	});
+
+});
+*/
